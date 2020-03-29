d41aa4d797092c9fc1c292a6d509db9f48d11a00
Peter Baumgartner <5107405+pmbaumgartner@users.noreply.github.com>
2020-03-29 14:19:37 -0400

    typing updates

--- model/social_distancing.py
+++ model/social_distancing.py
@@ -1,15 +1,43 @@
-from mesa import Agent, Model
-from mesa.time import RandomActivation
-from mesa.space import ContinuousSpace
+import enum
+from dataclasses import dataclass, asdict
+from typing import List, Tuple
 
-from redis.client import Redis
 import numpy as np
+from mesa import Agent, Model
+from mesa.space import ContinuousSpace
+from mesa.time import RandomActivation
+from redis.client import Redis
 
-from typing import Tuple, List, Dict
+
+class State(enum.Enum):
+    healthy = "healthy"
+    infected = "infected"
+    recovered = "recovered"
+
+
+@dataclass
+class FlatAgentData:
+    """Represents agent data for saving to Redis.
+
+    `social_distancing` gets converted to an int"""
+
+    unique_id: int
+    state: State
+    x: float
+    y: float
+    social_distancing: bool
+    recovery_time: int
+
+    def __post_init__(self):
+        """Converts non-acceptable types for Redis"""
+        self.social_distancing = int(self.social_distancing)
+        self.state = self.state.value
 
 
 class SocialDistanceModel(Model):
-    """A model with some number of agents."""
+    """A model with some number of agents. The agents move around a continuous space
+    defined by width, height. The model starts with one infected agent that moves and infects
+    other agents if they enter within a specified radius."""
 
     def __init__(
         self,
@@ -31,7 +59,6 @@ class SocialDistanceModel(Model):
             ["num_agents", "width", "height", "p_stationary", "speed"]
         )
 
-        # Create agents
         for i in range(self.num_agents):
             x = self.random.random() * self.space.x_max
             y = self.random.random() * self.space.y_max
@@ -47,7 +74,7 @@ class SocialDistanceModel(Model):
                 social_distancing=is_social_distancing,
             )
             if i == 0:
-                a.state = "sick"
+                a.state = State.infected
                 a.social_distancing = False
             self.space.place_agent(a, (x, y))
             self.schedule.add(a)
@@ -65,7 +92,7 @@ class SocialDistanceModel(Model):
 
     def save_agent_data(self):
         """Saves all agent data to redis"""
-        agent_data = (a.collect_data() for a in self.schedule.agents)
+        agent_data = (asdict(a.collect_data()) for a in self.schedule.agents)
         pipe = self.db.pipeline()
         for data in agent_data:
             pipe.xadd(self.id, data)
@@ -82,7 +109,7 @@ class PersonAgent(Agent):
         pos: Tuple[float, float],
         speed: float,
         heading: Tuple[float, float],
-        state: str = "healthy",
+        state: State = State.healthy,
         social_distancing: bool = False,
     ):
         super().__init__(unique_id, model)
@@ -113,26 +140,31 @@ class PersonAgent(Agent):
 
         If they've been sick for 100 steps, they're recovered"""
 
-        if self.state == "sick":
+        if self.state == State.infected:
             self.recovery_time += 1
             if self.recovery_time == 100:
-                self.state = "recovered"
+                self.state = State.recovered
 
     def infect(self) -> None:
-        if self.state == "sick":
+        """If infected, find all neighbors within radius.
+
+        If any neighbors are in the healthy state, infect them."""
+        if self.state == State.infected:
             neighbors = self.model_space.get_neighbors(self.pos, 10)
             if neighbors:
                 for n in neighbors:
-                    if n.state == "healthy":
-                        n.state = "sick"
+                    if n.state == State.healthy:
+                        n.state = State.infected
 
-    def collect_data(self) -> Dict:
-        data = {
-            "unique_id": self.unique_id,
-            "state": self.state,
-            "x": self.pos[0],
-            "y": self.pos[1],
-            "social_distancing": int(self.social_distancing),
-            "recovery_time": self.recovery_time,
-        }
+    def collect_data(self) -> FlatAgentData:
+        data = FlatAgentData(
+            **{
+                "unique_id": self.unique_id,
+                "state": self.state,
+                "x": self.pos[0],
+                "y": self.pos[1],
+                "social_distancing": self.social_distancing,
+                "recovery_time": self.recovery_time,
+            }
+        )
         return data
